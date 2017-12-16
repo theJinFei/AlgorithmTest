fdd618a1246d9b8ee0aa8be6a1d23ab26ec39c9c
theJinFei <34594300+theJinFei@users.noreply.github.com>
2017-12-16 23:27:55 +0800

    Add files via upload

---
+++ template.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <stdio.h>
+#include <algorithm>
+#include <iomanip>
+#include <string>
+#include <cstdio>
+#include <string.h>
+#include <set>
+#include <cmath>
+#include <map>
+#include <stack>
+using namespace std;
+
+int main()
+{
+	freopen("in.txt", "r", stdin);
+	freopen("out.txt", "w", stdout);
+    return 0;
+}
